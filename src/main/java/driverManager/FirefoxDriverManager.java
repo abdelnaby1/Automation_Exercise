5af247c73f52de4494aa7fa640a125fb067e6203
abdelnaby1 <ahmedaabdelnaby471@gmail.com>
2023-08-21 22:09:49 +0300

    refactoring drivers manager

--- src/main/java/driverManager/FirefoxDriverManager.java
+++ src/main/java/driverManager/FirefoxDriverManager.java
@@ -1,8 +1,6 @@
 package driverManager;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -14,22 +12,20 @@ import java.net.URL;
 
 public class FirefoxDriverManager extends DriverManager{
     @Override
-    protected WebDriver createDriver(ExecutionType executionType,String executionTypeProperty,String host,String port) {
+    protected WebDriver createDriver() {
         result = Reporter.getCurrentTestResult();
         context = result.getTestContext();
-        if(executionType == ExecutionType.REMOTE || (executionType == ExecutionType.FROM_PROPERTIES
-                && executionTypeProperty.equalsIgnoreCase("remote"))){
+        if(DriverManager.executionTypeProperty.equalsIgnoreCase("remote")){
 
             try {
-                driver.set(new RemoteWebDriver(new URL("http://" + host + ":" + port + "/wd/hub"),
+                driver.set(new RemoteWebDriver(new URL("http://" + DriverManager.host + ":" + DriverManager.port + "/wd/hub"),
                         getFirefoxOptions()));
                 context.setAttribute("driver", driver.get());
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
 
-        } else if (executionType == ExecutionType.FROM_PROPERTIES
-                && executionTypeProperty.equalsIgnoreCase("local headless")) {
+        } else if (DriverManager.executionTypeProperty.equalsIgnoreCase("local headless")) {
             driver.set(new FirefoxDriver(getFirefoxOptions()));
         } else {
             driver.set(new FirefoxDriver());
