911badecb30f73ae2927eb240d2b8e31286b699d
abdelnaby1 <ahmedaabdelnaby471@gmail.com>
2023-08-29 21:03:11 +0300

    read retry flag and maxretry from property file

--- src/main/java/Listener/Retry.java
+++ src/main/java/Listener/Retry.java
@@ -5,17 +5,21 @@ import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public class Retry implements IRetryAnalyzer {
-    private static final int    maxTry = 3;
+    private static final int   maxTry = Integer.parseInt(System.getProperty("retry.maxTry"));
+    private static final Boolean retry = Boolean.valueOf(System.getProperty("retry"));
     private int count  = 0;
 
     @Override
     public boolean retry(ITestResult iTestResult) {
-        if (!iTestResult.isSuccess ()) {
-            if (this.count < maxTry) {
-                ExtentReport.info ("Retrying test " + iTestResult.getName () + " with status " + getResultStatusName (
-                        iTestResult.getStatus ()) + " for the " + (this.count + 1) + " time(s).");
-                this.count++;
-                return true;
+        if (retry){
+            if (!iTestResult.isSuccess ()) {
+                if (this.count < maxTry) {
+                    ExtentReport.info ("Retrying test " + iTestResult.getName () + " with status " + getResultStatusName (
+                            iTestResult.getStatus ()) + " for the " + (this.count + 1) + " time(s).");
+                    this.count++;
+                    return true;
+                }
+                return false;
             }
         }
         return false;
