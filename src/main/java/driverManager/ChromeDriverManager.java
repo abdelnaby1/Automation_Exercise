6be2334003309fab562b88630d2dee08d85cca36
abdelnaby1 <ahmedaabdelnaby471@gmail.com>
2023-09-14 11:29:25 +0300

    edit some configurations

--- src/main/java/driverManager/ChromeDriverManager.java
+++ src/main/java/driverManager/ChromeDriverManager.java
@@ -1,6 +1,5 @@
 package driverManager;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
