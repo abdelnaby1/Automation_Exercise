1a838db655dddaaec8388a1b3e32e70885c90ec3
abdelnaby1 <ahmedaabdelnaby471@gmail.com>
2023-09-19 21:54:38 +0300

    trying docker to run remotely on selenium grid

--- src/main/java/driverManager/ChromeDriverManager.java
+++ src/main/java/driverManager/ChromeDriverManager.java
@@ -22,7 +22,7 @@ public class ChromeDriverManager implements DriverManager {
 //             * 2- You need to have a .yml file to configure the network between the containers like that we have in the src/main/resource file "docker-compose_native.yml"
 //             * 3- open a terminal on the .yml file directory
 //             * 4- Enter the following command that will setup the containers (1 hub & 4 nodes) and run them automatically:
-//             * docker-compose -f docker-compose_native.yml up --scale chrome=4 --remove-orphans -d
+//             * docker compose -f docker-compose_native.yml up --scale chrome=4 --remove-orphans -d
 //             * 5- Enter the following command to check the running containers: docker ps
 //             * 6- open a browser and enter this url to see the grid :D http://localhost:4444/ui/index.html
 //             * 7- execute using this condition
